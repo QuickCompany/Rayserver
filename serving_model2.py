1d9c81afcb60f11290b3549217014c52c588a76b
sparkingdark <debomastet335@gmail.com>
2024-03-13 11:19:40 +0530

    added s3 image function

--- serving_model2.py
+++ serving_model2.py
@@ -1,5 +1,7 @@
 from starlette.requests import Request
+import os
 import ray
+import boto3
 import logging
 from ray import serve
 import layoutparser as lp
@@ -10,6 +12,10 @@ from flask import jsonify
 import cv2
 from paddleocr import PaddleOCR , PPStructure
 from label_studio_sdk.utils import parse_config
+from dotenv import load_dotenv
+
+# load_dotenv("/root/rayserver/.env")
+
 
 
 ray_serve_logger = logging.getLogger("ray.serve")
@@ -18,10 +24,22 @@ class Translator:
     def __init__(self):
         model_path = "/root/layout_parsing/finetuned-model/model_final.pth"
         config_path = "/root/layout_parsing/config.yml"
-        
+        hostname = "blr1.vultrobjects.com"
+        secret_key = "dfdfsfsdfsdfsdf"
+        access_key = "xdfdfdfdfdf"
+        self.labelstudio_api_url = "https://labelstudio.io/api/predictions/"
+        self.headers = {
+                    "Authorization":"Token fdfdf",
+                }
+        session = boto3.session.Session()
+        self.client = session.client('s3', **{
+                                "region_name": hostname.split('.')[0],
+                                "endpoint_url": "https://" + hostname,
+                                "aws_access_key_id": access_key,
+                                "aws_secret_access_key": secret_key
+                                })
         self.model = lp.models.Detectron2LayoutModel(
             config_path,
-
             model_path,
             extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8],
             label_map={0: "extra", 1: "title", 2: "text", 3:"formula",
@@ -40,13 +58,21 @@ class Translator:
         image = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
         
         return image
-    def get_image_from_labelstudio(self,task_id,image_path):
-        label_studio_url = "https://labelstudio.debras.in"
-        image_url = f'{label_studio_url}/api/tasks/{task_id}{image_path}'
-        ray_serve_logger.info(image_url)
-        response = requests.get(image_url)
-        image_data = response.content
-        return image_data
+    def get_previous_prediction_result_from_labelstudio(self,task_id):
+        res = requests.get(self.labelstudio_api_url+f"{task_id}",headers=self.headers)
+        if res.status_code == 200:
+            ray_serve_logger.info(res.json())
+            return res.json()
+        else:
+            return None
+    def get_image_from_s3(self,image_path):
+        ray_serve_logger.info(image_path)
+        image_name = os.path.basename(image_path)
+        response = self.client.get_object(Bucket="labelstudio-patent-images",Key=image_name)
+        image_data = response['Body'].read()
+        np_array = np.frombuffer(image_data, np.uint8)
+        image = cv2.imdecode(np_array, cv2.IMREAD_COLOR)
+        return image
     async def __call__(self, request:Request):
         if request.url.path == "/health":
             return {
@@ -75,8 +101,7 @@ class Translator:
                 task_id = task.get("id")
                 data = task.get("data")
                 image_path = data.get("image")
-                # image_data = cv2.imread(self.get_image_from_labelstudio(task_id,image_path))
-                image_data = cv2.imread("/root/rayserver/201617010090_page17.png")
+                image_data = self.get_image_from_s3(image_path)
                 img_width, img_height = image_data.shape[1], image_data.shape[0]
                 layout_predicted = self.model.detect(image_data)
                 ray_serve_logger.info(layout_predicted)
@@ -102,14 +127,21 @@ class Translator:
                     'score': score,
                     })
                     # ray_serve_logger.info(results)
-                res = requests.post("https://labelstudio.replace.io/api/predictions/",headers={
-                    "Authorization":"Token replace_token",
-                },json={
+                is_prediction_exist = True if self.get_previous_prediction_result_from_labelstudio(task_id=task_id) is not None else False
+                if is_prediction_exist:
+                   res = requests.put(self.labelstudio_api_url,headers=self.headers,json={
                     "result":results,
                     "score": score,
                     "model_version":"detectron2",
                     "task":task_id
                     })
+                else:
+                    res = requests.post(self.labelstudio_api_url,headers=self.headers,json={
+                        "result":results,
+                        "score": score,
+                        "model_version":"detectron2",
+                        "task":task_id
+                        })
                 ray_serve_logger.info(res.content)
                 predictions.append({
                     "result":results,
