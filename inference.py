14eeae3c444bf65648cf2211048301eb8c66a039
sparkingdark <debomastet335@gmail.com>
2024-03-20 08:31:26 +0530

    adding image upload

--- inference.py
+++ inference.py
@@ -29,7 +29,7 @@ class Label(str,Enum):
 
 class VultrImageUploader(object):
     def __init__(self) -> None:
-        load_dotenv("/home/debo/Rayserver/.env")
+        load_dotenv("/root/new_layoutmodel_training/merged_dataset/.env")
         self.hostname = os.getenv("HOST_URL")
         secret_key = os.getenv("VULTR_OBJECT_STORAGE_SECRET_KEY")
         access_key = os.getenv("VULTR_OBJECT_STORAGE_ACCESS_KEY")
@@ -41,23 +41,27 @@ class VultrImageUploader(object):
             "aws_access_key_id": access_key,
             "aws_secret_access_key": secret_key
         })
-    
-    def upload_image(self,image:bytes):
+
+    def upload_image(self, image: bytes):
         image_name = f"{str(uuid4())}.jpg"
-        self.client.upload_fileobj(io.BytesIO(image),self.figures_bucket,image_name, ExtraArgs={'ACL':'public-read'})
-        image_url = f"https://{self.hostname}/.{self.figures_bucket}/{image_name}"
+        self.client.upload_fileobj(io.BytesIO(
+            image), self.figures_bucket, image_name, ExtraArgs={'ACL': 'public-read'})
+        image_url = f"https://{self.hostname}/{self.figures_bucket}/{image_name}"
         return image_url
+
+
 class LayOutInference(object):
-    def __init__(self,model_path:str,config_path:str,extra_config:List,label_map: Dict[int,str]) -> None:
+    def __init__(self, model_path: str, config_path: str, extra_config: List, label_map: Dict[int, str]) -> None:
         self.model = lp.Detectron2LayoutModel(config_path, model_path,
-                    extra_config=extra_config,
-                    label_map=label_map)
-        self.ocr = PaddleOCR(use_angle_cls=True, lang='en')  # Specify the languages you want to support
-        
-        self.table_engine = PPStructure(lang='en',show_log=True , ocr=True)
+                                              extra_config=extra_config,
+                                              label_map=label_map)
+        # Specify the languages you want to support
+        self.ocr = PaddleOCR(use_angle_cls=True, lang='en')
+
+        self.table_engine = PPStructure(lang='en', show_log=True, ocr=True)
         self.vultr_img_uploader = VultrImageUploader()
-    
-    def remove_html_body_tags(self,html_string):
+
+    def remove_html_body_tags(self, html_string):
         soup = BeautifulSoup(html_string, 'html.parser')
         # Remove <html> and <body> tags
         if soup.html:
@@ -66,12 +70,19 @@ class LayOutInference(object):
             soup.body.unwrap()
         return str(soup)
 
-    def do_inference(self,pdf_link):
+    def convert_image_to_byte(self, image):
+        byte_io = io.BytesIO()
+        image.save(byte_io, format="JPEG")
+        cropped_image_bytes = byte_io.getvalue()
+
+        return cropped_image_bytes
+
+    def do_inference(self, pdf_link):
         t1 = time.perf_counter()
         pdf = requests.get(pdf_link).content
         pages = convert_from_bytes(pdf)
         html_code = """"""
-        for count,page in enumerate(pages):
+        for count, page in enumerate(pages):
             layout_predicted = self.model.detect(page)
             for block in layout_predicted._blocks:
                 if block.type == Label.EXTRA.value:
@@ -83,13 +94,16 @@ class LayOutInference(object):
                     html_code += f"\n<p>{text}</p>"
                 elif block.type == Label.FORMULA.value:
                     print(Label.FORMULA)
-                    url = self.vultr_img_uploader.upload_image(np.array(page.crop((block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))).tobytes())
+                    url = self.vultr_img_uploader.upload_image(self.convert_image_to_byte(
+                        page.crop((block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))))
                     html_code += f"\n<img src=\"{url}\">"
                 elif block.type == Label.TABLE.value:
-                    results = self.table_engine(np.array(page.crop((block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))))
+                    results = self.table_engine(np.array(page.crop(
+                        (block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))))
                     for table in results:
                         table_html = table['res']['html']
-                        preprocessed_table_html = self.remove_html_body_tags(table_html)
+                        preprocessed_table_html = self.remove_html_body_tags(
+                            table_html)
                         print(preprocessed_table_html)
                         html_code += f"{preprocessed_table_html}"
                 elif block.type == Label.LIST.value:
@@ -104,20 +118,24 @@ class LayOutInference(object):
                     print(text)
                     html_code += f"\n<h3>{text}</h3>"
                 elif block.type == Label.FIGURE.value:
-                    url = self.vultr_img_uploader.upload_image(np.array(page.crop((block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))).tobytes())
+                    url = self.vultr_img_uploader.upload_image(self.convert_image_to_byte(
+                        page.crop((block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))))
                     html_code += f"<img src=\"{url}\">"
-                
+
         end_time = time.perf_counter() - t1
         print(f"time taken: {end_time}")
         return html_code
 
     def extract_text(self, page, block):
-        cropped_img = page.crop((block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))
+        cropped_img = page.crop(
+            (block.block.x_1, block.block.y_1, block.block.x_2, block.block.y_2))
         img_array = np.array(cropped_img)
-        img_array = img_array[:, :, ::-1]  # Rearrange color channels for RGB if needed
+        # Rearrange color channels for RGB if needed
+        img_array = img_array[:, :, ::-1]
         ocr_results = self.ocr.ocr(img_array)
         return ocr_results
-    def extract_text_from_paddocr_output(self,output):
+
+    def extract_text_from_paddocr_output(self, output):
         """
         This function extracts text from the specific format of paddleocr output.
 
@@ -131,8 +149,8 @@ class LayOutInference(object):
         for block in output:
             # Extract coordinates and text from the block
             for inner in block:
-                _,text_arr = inner
-                in_text,conf = text_arr
+                _, text_arr = inner
+                in_text, conf = text_arr
                 text += in_text
         return text.strip()
 
