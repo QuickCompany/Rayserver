1e787cd3492ee812a530a0744b70e8ef3ddf89a3
sparkingdark <debomastet335@gmail.com>
2024-04-10 03:56:27 +0000

    added functions

--- rayapproaches/functional_ray_actor.py
+++ rayapproaches/functional_ray_actor.py
@@ -27,7 +27,6 @@ import easyocr
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from itertools import chain
 
-
 logger = logging.getLogger("ray.serve")
 
 
@@ -64,10 +63,10 @@ def load_layout():
     )
     return layout_model
 
-model = load_easyocr()
-layout_model = load_layout()
-layout_model_ref = ray.put(layout_model)
-model_ref = ray.put(model)
+# model = load_easyocr()
+# # layout_model = load_layout()
+# # layout_model_ref = ray.put(layout_model)
+# model_ref = ray.put(model)
 
 @ray.remote(num_gpus=0.1, concurrency_groups={"io": 2, "compute": 10})
 class Layoutinfer:
@@ -105,6 +104,15 @@ class EasyOcr:
         except Exception as e:
             logger.info(e)
 
+@ray.remote(num_gpus=0.5)
+def process_image(model_ref,image_data):
+    try:
+        # text = model.readtext(np.array(image_data),detail=0,paragraph=True)
+        text = model_ref.readtext_batched(image_data,n_height=800,n_width=600)
+        return text
+    except Exception as e:
+        logger.info(e)
+
 @ray.remote(num_gpus=0.1)
 def detect_image(layout_model,image):
     prediction = layout_model.detect(image)
@@ -115,7 +123,7 @@ def split_into_batches(lst, batch_size):
     for i in range(0, len(lst), batch_size):
         yield lst[i:i+batch_size]
 
-@ray.remote
+@ray.remote(num_cpus=1)
 def get_pdf_text(req: Tuple):
         page, layout_predicted = req
         remaining_list = []
@@ -135,27 +143,31 @@ slug = url_json.get("slug")
 pdf = requests.get(link).content
 pdf = convert_from_bytes(pdf, thread_count=10)
 layout_model = Layoutinfer.remote()
-easyocr_model = EasyOcr.remote()
-easyocr_model_2 = EasyOcr.remote()
 start_time = time.time()
 cor_1 =list(ray.get([layout_model.detect.remote(i) for i in pdf]))
 logger.info(cor_1)
 page_pred = list(zip(pdf,cor_1))
 
 ray.kill(layout_model)
+easyocr_model = EasyOcr.remote()
+# easyocr_model_2 = EasyOcr.remote()
+
 results = list(chain.from_iterable(list(ray.get([get_pdf_text.remote(i) for i in page_pred]))))
+
 print(len(results))
 result_in_batch = split_into_batches(results,20)
 
 
-pool = ActorPool([easyocr_model,easyocr_model_2])
-# res = ray.get([easyocr_model.process_image.remote(i) for i in result_in_batch])
+pool = ActorPool([easyocr_model])
+t1 = time.time()
 
+# res = ray.get([process_image.remote(model_ref,i) for i in result_in_batch])
+print(f"time take:{time.time()-t1}")
 
 
 for result in pool.map(lambda a,v:a.process_image.remote(v),result_in_batch):
     print(result)
-
+# print(f"time take:{time.time()-t1}")
 # app: serve.Application = LayoutRequest.bind()
 # serve.run(name="newapp", target=app,
 #           route_prefix="/", host="0.0.0.0", port=8000)
